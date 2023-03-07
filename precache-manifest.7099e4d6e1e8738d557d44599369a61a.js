b0d5259d75661f808f961688d4dc993a0ec30aac
Avyukt <avyuktrkumar08@gmail.com>
2023-03-07 20:01:06 +0530

    Updates

---
+++ precache-manifest.7099e4d6e1e8738d557d44599369a61a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "045f0fdc3823888295fe",
+    "url": "/KBC_React_App/static/css/main.ffd07770.chunk.css"
+  },
+  {
+    "revision": "045f0fdc3823888295fe",
+    "url": "/KBC_React_App/static/js/main.045f0fdc.chunk.js"
+  },
+  {
+    "revision": "1d6a2b3221ccfbd36487",
+    "url": "/KBC_React_App/static/js/1.1d6a2b32.chunk.js"
+  },
+  {
+    "revision": "c42e9d19a65acf66ab5e",
+    "url": "/KBC_React_App/static/js/2.c42e9d19.chunk.js"
+  },
+  {
+    "revision": "c9194ffb29cdb0cceed1",
+    "url": "/KBC_React_App/static/js/3.c9194ffb.chunk.js"
+  },
+  {
+    "revision": "8628c9ab05712edb6b50",
+    "url": "/KBC_React_App/static/js/runtime~main.8628c9ab.js"
+  },
+  {
+    "revision": "e55862ad3acb9ac8ac4454221366f389",
+    "url": "/KBC_React_App/static/media/src_sounds_wrong.e55862ad.mp3"
+  },
+  {
+    "revision": "3d8ba0a6804c784263ebab21dc1f4027",
+    "url": "/KBC_React_App/static/media/src_sounds_play.3d8ba0a6.mp3"
+  },
+  {
+    "revision": "d27f45adf88f4e70e787423e4db695ad",
+    "url": "/KBC_React_App/static/media/src_sounds_correct.d27f45ad.mp3"
+  },
+  {
+    "revision": "889b8ea9616d69c19dbd3754aea454e5",
+    "url": "/KBC_React_App/static/media/bg.889b8ea9.jpg"
+  },
+  {
+    "revision": "37a01be42740f3ffb9c163037ce36eb1",
+    "url": "/KBC_React_App/index.html"
+  }
+];
